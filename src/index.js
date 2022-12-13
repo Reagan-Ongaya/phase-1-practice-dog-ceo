ea640787e9859cbda385ede07801d48a80dfcf4f
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-14 02:01:42 +0300

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch('imgUrl')
+  .then(function(response){
+    console.log (response)
+  })
+
+.then(function(data) {
+    console.log(data)
+});
